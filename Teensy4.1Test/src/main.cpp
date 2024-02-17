b1ccba1d5ddede817b2fef11e519c5a9320567cc
Edison Wong <edisonwong2004@gmail.com>
2024-02-16 19:40:55 -0500

    added tests for the bmp388 and the led on the teensy, default examples, will test tonight/tomorrow

--- Teensy4.1Test/src/main.cpp
+++ Teensy4.1Test/src/main.cpp
@@ -1,11 +1,52 @@
 #include <Arduino.h>
 
+//Used for the BMP388 sensor definitions
+#include <Adafruit_BMP3XX.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 3
+
+//extra CS pins for the other sensors
+#define ADXL375_CS 7
+#define LSM_CS 9
+#define LIS3MDL_CS 5
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
+
+//End of BMP388 sensor definitions
+
 //Blink Example, Turns an LED on for one second, then off for one second, repeatedly.
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  /*
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
+
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  */
 }
 
 // the loop function runs over and over again forever
@@ -16,23 +57,25 @@ void loop() {
   delay(1000);                      // wait for a second
   printf("Hello World\n");
   Serial.println("World Hello");
-}
 
-/*
-// put function declarations here:
-int myFunction(int, int);
+  /*
+  if (! bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println();
+  delay(2000);
+  */
 }
-*/
